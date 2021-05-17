4c36afb5842be54f5a332887581d5cdde638d6a7
Paul Rosero <paul.rosero23@gmail.com>
2021-05-16 21:31:55 -0400

    add line 1 since it was deleted by accident

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
+import Login from './UserForms/Login';
+import Signup from './UserForms/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
-    </div>
+       <h1>Welcome</h1>
+       <Signup />
+      <Login />
+     </div>
   );
 }
 
