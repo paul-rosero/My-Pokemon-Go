6730b6a932b017102cc68cd9e11f1b0a0769ab7f
Paul Rosero <paul.rosero23@gmail.com>
2021-05-24 21:06:02 -0400

    change how port is called

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.DB_PORT || 3001;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Server is up and running on ${ port }`)
