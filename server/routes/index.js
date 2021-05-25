4a4588152fc47848b8ffe347582f5891d513ba23
Paul Rosero <paul.rosero23@gmail.com>
2021-05-24 21:06:31 -0400

    refactor code to be able to use other routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
+const users = require('./users');
+// const pokemons = require('./pokemons');
 
-router.get('/api/v1/', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).send({
     success: 'true',
     message: 'Welcome',
     version: '1.0.0',
   });
 });
+
+router.use(users);
+// router.use('/pokemons', pokemons);
 
 module.exports = router;
