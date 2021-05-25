c97725644436f95001fd869accf5d6b01681fd19
Paul Rosero <paul.rosero23@gmail.com>
2021-05-24 21:05:31 -0400

    change code to see how everything functions

--- server/app.js
+++ server/app.js
@@ -3,17 +3,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const index = require('./routes/index');
-const usersRoute = require('./routes/users');
+// const usersRoute = require('./routes/users');
 // const pokemonsRoute = require('./routes/pokemons');
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.json({ type: 'application/vnd.api+json' }));
+// app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
-app.use(index);
-// app.use(usersRoute)
-app.use('/api/v1', usersRoute);
+app.use('/api/v1', index);
+// app.use('/api/v1', usersRoute);
 // app.use('/api/v1', pokemonRoute);
 
 module.exports = app;
