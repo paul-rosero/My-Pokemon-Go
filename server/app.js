fb209ea7b2234454d178e5f82fc2e2c4164cc6f8
Paul Rosero <paul.rosero23@gmail.com>
2021-06-04 12:00:41 -0400

    untrack the file before you add a rule to ignore it

--- server/app.js
+++ server/app.js
@@ -1,15 +1,33 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const index = require('./routes/index');
 // const usersRoute = require('./routes/users');
 // const pokemonsRoute = require('./routes/pokemons');
+const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.json({ type: 'application/vnd.api+json' }));
-app.use(cors());
+app.use(cookieParser());
+app.use(session({
+    key: "userId",
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60 * 60 * 24
+    }
+}));
+
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
 
 app.use('/api/v1', index);
 // app.use('/api/v1', usersRoute);
