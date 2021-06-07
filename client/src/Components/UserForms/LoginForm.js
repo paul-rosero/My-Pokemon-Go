3548cdd21838e3021644822ee9d70c4c95b6ffe8
Paul Rosero <paul.rosero23@gmail.com>
2021-06-07 19:54:41 -0400

    clean up code that did nothing to fix the network error

--- client/src/Components/UserForms/LoginForm.js
+++ client/src/Components/UserForms/LoginForm.js
@@ -22,39 +22,12 @@ const LoginForm = () => {
     };
 
     useEffect(() => {
-        try {
-            axios.get('http://localhost:5000/api/v1/users/login')
-                .then(({ data }) => {
-                    if (data.loggedIn === true) {
-                        setLoggedIn(data.user[0])
-                    }; 
-                })    
-        } catch (error) {
-                if (axios.isAxiosError(error)) {
-                    
-                  } else {
-                   
-                  }
-            }
-       
-    //    .catch(error => {
-    //         if (error.response) {
-    //             // The request was made and the server responded with a status code
-    //             // that falls out of the range of 2xx
-    //             console.log(error.response.data);
-    //             console.log(error.response.status);
-    //             console.log(error.response.headers);
-    //         } else if (error.request) {
-    //             // The request was made but no response was received
-    //             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-    //             // http.ClientRequest in node.js
-    //             console.log(error.request.onreadystatechange());
-    //         } else {
-    //             // Something happened in setting up the request that triggered an Error
-    //             console.log('Error', error.message);
-    //         }
-    //         console.log(error.config);
-    //     })
+        axios.get('http://localhost:5000/api/v1/users/login')
+        .then(({ data }) => {
+            if (data.loggedIn === true) {
+                setLoggedIn(data.user[0])
+            }; 
+        })    
     }, []);
 
     return (
